3f594a501c2c49c60db0f651ec849ab5650b5a91
aurelio-soares <aurelio.soares@rgb.com.br>
2022-09-20 14:32:43 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="segunda_pagina.php" method="POST">
+    <label for="nome">NOME</label>
+    <input type="text" name="nome"><br>
+
+    <label for="senha">SENHA</label>
+    <input type="password" name="senha"><br>
+
+    <label for="email">E-MAIL</label>
+    <input type="email" name="email"><br>
+  </form>
+</body>
+
+</html>
---
+++ segunda_pagina.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <ul>
+    <li>Nome: <?php echo $_POST['nome'] ?></li>
+    <li>Senha: <?php echo $_POST['senha'] ?></li>
+    <li>Email: <?php echo $_POST['email'] ?></li>
+  </ul>
+</body>
+
+</html>
