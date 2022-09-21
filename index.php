6067409b636c007a4d9a37e674fb180605a87566
aurelio-soares <aurelio.soares@rgb.com.br>
2022-09-21 09:28:24 -0300

    validação do formulário

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
@@ -9,18 +9,58 @@
 </head>
 
 <body>
-  <form action="segunda_pagina.php" method="POST">
-    <label for="nome">NOME</label>
-    <input type="text" name="nome"><br>
+  <h1>Exmeplo de formulário com validação</h1>
+  <span>*Campos obrigatórios.</span> <br><br>
+  <form action="index.php" method="POST">
+    <label for="nome">NOME:*</label>
+    <input type="text" name="nome"><br><br>
 
-    <label for="senha">SENHA</label>
-    <input type="password" name="senha"><br>
+    <label for="senha">SENHA:*</label>
+    <input type="text" name="senha"><br><br>
 
-    <label for="email">E-MAIL</label>
-    <input type="email" name="email"><br>
+    <label for="email">E-MAIL:*</label>
+    <input type="text" name="email"><br><br>
 
-    <button type="submit">ENVIAR</button>
+    <label for="genero">GÊNERO:*</label>
+    <input type="radio" name="genero" value="masculino">Masculino
+    <input type="radio" name="genero" value="feminino">Feminino
+    <input type="radio" name="genero" value="outro">Outro <br><br>
+
+    <button name="enviado" type="submit">ENVIAR</button>
   </form>
+
+  <?php if (isset($_POST['enviado'])) {
+
+    $genero = "Não informado";
+    if (isset($_POST['genero'])) {
+      $genero = $_POST['genero'];
+    } ?>
+
+  <!-- VALIDAÇÃO DO FORMULÁRIO -->
+  <?php if (empty($_POST['nome']) || strlen($_POST['nome']) < 3 || strlen($_POST['nome']) > 100) { ?>
+  <p>Por favor insira um nome válido.</p>
+  <?php } ?>
+  <?php if (empty($_POST['senha']) || strlen($_POST['senha']) < 3 || strlen($_POST['senha']) > 16) { ?>
+  <p>Por favor insira uma senha de 3 a 16 caracteres.</p>
+  <?php } ?>
+  <?php if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) { ?>
+  <p>Por favor insira um e-mail válido.</p>
+  <?php } ?>
+  <?php if ($genero != 'masculino' && $genero != 'feminino' && $genero != 'outro') {
+      unset($genero)  ?>
+  <p>Por favor selecione um gênero dentre as opções apresentadas.</p>
+  <?php } ?>
+
+  <?php if (!empty($_POST['nome']) && !empty($_POST['senha']) && !empty($_POST['email']) && !empty($_POST['genero'])) { ?>
+  <ul>
+    <li>Nome: <?php echo $_POST['nome'] ?></li>
+    <li>Senha: <?php echo $_POST['senha'] ?></li>
+    <li>Email: <?php echo $_POST['email'] ?></li>
+    <li>Gênero: <?php echo $genero ?></li>
+  </ul>
+
+  <?php }
+  } ?>
 </body>
 
 </html>
